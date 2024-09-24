7413bdec96f92e4fc8c3e9dc8ff17cbe3d2c17f2
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-09-24 20:45:01 +0100

    complet the backend

--- server/routes.js
+++ server/routes.js
@@ -21,6 +21,12 @@ router.post("/tasks", async (req, res) => {
   const collection = getCollection();
   const { task } = req.body;
 
+  if (!task) {
+    return res.status(400).json({ mssg: "Error no task found" });
+  }
+
+  task = JSON.stringify(task);
+
   const newTask = await collection.insertOne({ task, status: false });
 
   res.status(201).json({ task, status: false, _id: newTask.insertedId });
@@ -41,6 +47,10 @@ router.put("/tasks/:id", async (req, res) => {
   const collection = getCollection();
   const _id = new ObjectId(req.params.id);
   const { status } = req.body;
+
+  if (typeof status !== "boolean") {
+    return res.status(400).json({ mssg: "Invalid status" });
+  }
 
   const updatedTask = await collection.updateOne(
     { _id },
