c882129a89ccb75c4e10671dd209cb4b3e1fbd31
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-09-25 19:47:32 +0100

    start deployment create the build directory

--- server/app.js
+++ server/app.js
@@ -3,9 +3,21 @@ import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes.js";
 import { connectToMongoDB } from "./database.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
 
 dotenv.config();
 
